a136a34d5573150ec85201d7be0cb4580b7c6622
ladl1983 <Pet1github2erL!>
2020-03-27 19:48:04 +0100

    update

--- pandemie.js
+++ pandemie.js
@@ -1,8 +1,122 @@
+let npersons = 200;
+let start_infected = 0.01;
+let ninfected = parseInt(npersons * start_infected);
+let persons = [];
+let infected = [];
+let w = 840;
+let h = 480;
+let trans_prob = 0.5;
+let slider_behaviour;
+let temp_slider_value;
+let slider_inf_behaviour;
+let temp_inf_slider_value;
+let ratios = [];
+let counter = 0;
+
 function setup() {
-    createCanvas(840, 480);
-    background(51);
-    //slider = createSlider(1, 10, 1);
-    // for (let i = 0; i < TOTAL; i++) {
-    //   birds[i] = new Bird();
-    // }
+  createCanvas(w, h);
+  background(100);
+  //slider = createSlider(1, 10, 1);
+  // for (let i = 0; i < TOTAL; i++) {
+  //   birds[i] = new Bird();
+  // }
+  slider_behaviour = createSlider(0, 1, 0.9, 0.01);
+  slider_behaviour.position(20, 20);
+  temp_slider_value = slider_behaviour.value()
+  slider_inf_behaviour = createSlider(0, 300, 200,5);
+  slider_inf_behaviour.position(20, 40);
+  temp_inf_slider_value = slider_inf_behaviour.value()
+
+  for (let n = 0; n <= npersons - ninfected; n++) {
+    let quarantine = false;
+    if (random() < temp_slider_value) {
+      quarantine = true;
+    }
+    persons[n] = new Person(random(w), random(h), false, quarantine, temp_inf_slider_value);
   }
+  let k = 0
+  for (let n = npersons - ninfected; n <= npersons; n++) {
+    infected[k] = new Person(random(w), random(h), true, false, temp_inf_slider_value);
+    k += 1
+  }
+}
+
+
+function draw() {
+
+  counter += 1;
+  if (slider_behaviour.value() != temp_slider_value) {
+    reset();
+  }
+  if (slider_inf_behaviour.value() != temp_inf_slider_value)
+  {
+    reset();
+  }
+  background(150);
+
+
+  for (let person of persons) {
+    person.show();
+    person.move();
+  }
+  fill(255, 255, 255);
+  noStroke();
+  text(temp_slider_value, slider_behaviour.x * 2 + slider_behaviour.width - 25, 30);
+  text('Stay at home rate for healthy', slider_behaviour.x * 2 + slider_behaviour.width, 30);
+  text(temp_inf_slider_value, slider_behaviour.x * 2 + slider_behaviour.width - 25, 50);
+  text('Time infected people run around', slider_behaviour.x * 2 + slider_behaviour.width, 50);
+
+  for (let person of infected) {
+    person.show();
+    person.move();
+  }
+
+  if (counter % 5 == 0) {
+    ratios.push([persons.length / (npersons + ninfected), infected.length / (npersons + ninfected)]);
+    if (ratios.length > 300) {
+      ratios.shift();
+    }
+    
+  }
+  let posx = 0;
+    for (let ratio of ratios) {
+      stroke(0, 0, 255);
+      line(posx, 480, posx, 480 - ratio[0] * 50);
+      stroke(255, 0, 0);
+      line(posx, 480 - ratio[0] * 50, posx, 480 - 50);
+      posx += 1;
+    }
+
+  for (let i = infected.length - 1; i >= 0; i--) {
+    for (let p = persons.length - 1; p >= 0; p--) {
+      infected[i].contact(persons[p])
+      if (persons[p].infected) {
+        infected.push(persons.splice(p, 1)[0]);
+      }
+    }
+  }
+
+  console.log(temp_inf_slider_value);
+}
+
+function reset() {
+  counter = 0;
+  ratios = [];
+  persons = [];
+  infected = [];
+  temp_slider_value = slider_behaviour.value()
+  temp_inf_slider_value = slider_inf_behaviour.value()
+
+  for (let n = 0; n <= npersons - ninfected; n++) {
+    let quarantine = false;
+    if (random() < temp_slider_value) {
+      quarantine = true;
+    }
+    persons[n] = new Person(random(w), random(h), false, quarantine,temp_inf_slider_value);
+  }
+  let k = 0
+  for (let n = npersons - ninfected; n <= npersons; n++) {
+    infected[k] = new Person(random(w), random(h), true, false,temp_inf_slider_value);
+    k += 1
+  }
+}
--- person.js
+++ person.js
@@ -1,21 +1,71 @@
 
 class Person {
-    constructor(x,y,infected, quarantine) {
+    constructor(x, y, infected, quarantine,inf_behaviour) {
         this.x = x;
         this.y = y;
         this.quarantine = quarantine;
         this.infected = infected;
+        let direction = random(0, 200) * 2 * PI / 200;
+        this.speedx = cos(direction);
+        this.speedy = sin(direction);
+        this.counter = 0;
+        this.inf_behaviour = inf_behaviour;
     }
 
+    size = 10;
     show() {
-        if(this.infected){
-            //stroke(255);
-            fill(255, 0,0);
-            ellipse(this.x, this.y, 10, 10);
+        if (this.infected) {
+            stroke(255);
+            fill(255, 0, 0);
+            ellipse(this.x, this.y, this.size, this.size);
         }
         else {
-            fill(0, 0,255);
-            ellipse(this.x, this.y, 10, 10);
+            stroke(255);
+            noStroke();
+            fill(0, 0, 255);
+            ellipse(this.x, this.y, this.size, this.size);
         }
+    }
+
+    move() {
+        if (!(this.quarantine)) {
+            this.x += this.speedx;
+            this.y += this.speedy;
+            if (this.infected) {
+                this.counter += 1;
+            }
+            if ((this.counter > this.inf_behaviour) && this.infected) {
+                this.quarantine = true;
+            }
+
+        }
+        if (this.x < 0) {
+            this.speedx *= -1;
+        }
+        if (this.y < 0) {
+            this.speedy *= -1;
+        }
+        if (this.x > w) {
+            this.speedx *= -1;
+        }
+        if (this.y > h) {
+            this.speedy *= -1;
+        }
+
+    }
+
+    dist(x, y) {
+        return Math.sqrt((this.x - x) * (this.x - x) + (this.y - y) * (this.y - y))
+    }
+
+    // check contact, if distance is small the transmission of disease is dependent on transmission probabilty
+    contact(person) {
+
+        if (this.dist(person.x, person.y) < this.size) {
+            if (random() > trans_prob) {
+                person.infected = true;
+            }
+        }
+
     }
 }
